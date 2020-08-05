ad59882a0573eba38f674dbc7b84f1f42989a08e
shrenik0123 <46769965+shrenik0123@users.noreply.github.com>
2020-08-05 18:03:24 +0530

    Completed settings page UI

--- TheGoodFridge/TheGoodFridge/Views/SettingsView.swift
+++ TheGoodFridge/TheGoodFridge/Views/SettingsView.swift
@@ -14,6 +14,15 @@ class SettingsView: UIView {
     let blankLabel = UILabel()
     let appSupportLabel = UILabel()
     
+    let accDetailsButton = UIButton()
+    let currChallengesButton = UIButton()
+    let helpButton = UIButton()
+    let privacyButton = UIButton()
+    let signOutButton = UIButton()
+    
+    let blankViewAboveAppSupport = UIView()
+    let blankViewAboveSignOut = UIView()
+    
     let fullScreenView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
@@ -89,6 +98,9 @@ class SettingsView: UIView {
         
         blankLabel.translatesAutoresizingMaskIntoConstraints = false
         setLabels()
+        setButtonViews()
+        setBlankViews()
+        changeSignOutButton()
         
         addSubview(fullScreenView)
         fullScreenView.addSubview(wrapperView)
@@ -99,8 +111,14 @@ class SettingsView: UIView {
         wrapperView.addSubview(stackView)
         
         stackView.addArrangedSubview(myAccountLabel)
-        stackView.addArrangedSubview(blankLabel)
+        stackView.addArrangedSubview(accDetailsButton)
+        stackView.addArrangedSubview(currChallengesButton)
+        stackView.addArrangedSubview(blankViewAboveAppSupport)
         stackView.addArrangedSubview(appSupportLabel)
+        stackView.addArrangedSubview(helpButton)
+        stackView.addArrangedSubview(privacyButton)
+        stackView.addArrangedSubview(blankViewAboveSignOut)
+        stackView.addArrangedSubview(signOutButton)
         
         
 //        settingsStack.addArrangedSubview(backButton)
@@ -146,10 +164,32 @@ class SettingsView: UIView {
             settingsSymbol.trailingAnchor.constraint(equalTo: wrapperView.trailingAnchor, constant: -34),
             settingsSymbol.leadingAnchor.constraint(equalTo: wrapperView.trailingAnchor, constant: -60),
             
-            stackView.topAnchor.constraint(equalTo: settingsLabel.bottomAnchor, constant: 70),
+            stackView.topAnchor.constraint(equalTo: settingsLabel.bottomAnchor, constant: 50),
             stackView.leadingAnchor.constraint(equalTo: wrapperView.leadingAnchor),
             stackView.trailingAnchor.constraint(equalTo: wrapperView.trailingAnchor),
             stackView.bottomAnchor.constraint(equalTo: wrapperView.safeAreaLayoutGuide.bottomAnchor, constant: -23),
+            
+            accDetailsButton.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            accDetailsButton.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+            
+            currChallengesButton.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            currChallengesButton.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+            
+            helpButton.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            helpButton.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+            
+            privacyButton.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            privacyButton.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+            
+            
+            blankViewAboveAppSupport.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            blankViewAboveAppSupport.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+            
+            blankViewAboveSignOut.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            blankViewAboveSignOut.trailingAnchor.constraint(equalTo: stackView.trailingAnchor),
+            
+            signOutButton.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
+            signOutButton.trailingAnchor.constraint(equalTo: stackView.trailingAnchor)
             
 //            myAccountLabel.leadingAnchor.constraint(equalTo: stackView.leadingAnchor),
 //            myAccountLabel.heightAnchor.constraint(equalTo: stackView.heightAnchor, multiplier: 1/3),
@@ -206,9 +246,140 @@ class SettingsView: UIView {
     }
     
     func setLabels() {
-        changeLabel(label: myAccountLabel, text: "     MY ACCOUNT", font: UIFont(name: "Amiko-Bold", size: 14)!, color: #colorLiteral(red: 0.3098039216, green: 0.3098039216, blue: 0.3098039216, alpha: 1))
-        changeLabel(label: appSupportLabel, text: "    APP SUPPORT", font: UIFont(name: "Amiko-Bold", size: 14)!, color: #colorLiteral(red: 0.3098039216, green: 0.3098039216, blue: 0.3098039216, alpha: 1))
+        changeLabel(label: myAccountLabel, text: "          MY ACCOUNT", font: UIFont(name: "Amiko-Bold", size: 14)!, color: #colorLiteral(red: 0.3098039216, green: 0.3098039216, blue: 0.3098039216, alpha: 1))
+        changeLabel(label: appSupportLabel, text: "          APP SUPPORT", font: UIFont(name: "Amiko-Bold", size: 14)!, color: #colorLiteral(red: 0.3098039216, green: 0.3098039216, blue: 0.3098039216, alpha: 1))
     }
     
+    func changeButtonView(button: UIButton, text: String) {
+        let view = UIView()
+        //let lineView = UIView()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        view.translatesAutoresizingMaskIntoConstraints = false
+//        lineView.translatesAutoresizingMaskIntoConstraints = false
+//        lineView.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.37)
+        let label = UILabel()
+        changeLabel(label: label, text: text, font: UIFont(name: "Amiko-Regular", size: 14)!, color: #colorLiteral(red: 0.3098039216, green: 0.3098039216, blue: 0.3098039216, alpha: 1))
+        
+        let img = UIImageView()
+        let imgFrame = CGRect(x: 300, y: 25, width: 7.5, height: 12)
+        img.image = UIImage(named: "NextButton")
+        img.frame = imgFrame
+        img.contentMode = .scaleAspectFill
+        
+        view.addBorders(edges: [.top], color: UIColor(red: 0, green: 0, blue: 0, alpha: 0.37))
+        
+        button.addSubview(view)
+        //view.addSubview(lineView)
+        view.addSubview(label)
+        view.addSubview(img)
+        
+        let constraints = [
+            view.topAnchor.constraint(equalTo: button.topAnchor),
+            view.bottomAnchor.constraint(equalTo: button.bottomAnchor),
+            view.leadingAnchor.constraint(equalTo: button.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: button.trailingAnchor),
+            
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            label.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            
+//            lineView.topAnchor.constraint(equalTo: view.topAnchor),
+//            lineView.bottomAnchor.constraint(equalTo: view.topAnchor, constant: 1),
+//            lineView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            lineView.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
+        
+    }
     
+    func changeSignOutButton() {
+        let view = UIView()
+        //let lineView = UIView()
+        signOutButton.translatesAutoresizingMaskIntoConstraints = false
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor(red: 0.855, green: 0.925, blue: 0.824, alpha: 1)
+
+        let label = UILabel()
+        changeLabel(label: label, text: "SIGN OUT", font: UIFont(name: "Amiko-Bold", size: 14)!, color: UIColor(red: 0.424, green: 0.683, blue: 0.305, alpha: 1))
+        
+        view.addBorders(edges: [.top, .bottom], color: UIColor(red: 0, green: 0, blue: 0, alpha: 0.37))
+        
+        signOutButton.addSubview(view)
+        //view.addSubview(lineView)
+        view.addSubview(label)
+                
+        let constraints = [
+            view.topAnchor.constraint(equalTo: signOutButton.topAnchor),
+            view.bottomAnchor.constraint(equalTo: signOutButton.bottomAnchor),
+            view.leadingAnchor.constraint(equalTo: signOutButton.leadingAnchor),
+            view.trailingAnchor.constraint(equalTo: signOutButton.trailingAnchor),
+        
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(constraints)
+    }
+    
+    func setButtonViews() {
+        changeButtonView(button: accDetailsButton, text: "             ACCOUNT DETAILS")
+        changeButtonView(button: currChallengesButton, text: "             CURRENT CHALLENGES")
+        changeButtonView(button: helpButton, text: "             HELP")
+        changeButtonView(button: privacyButton, text: "             PRIVACY POLICY")
+        
+        
+    }
+    
+    func setBlankViews() {
+        blankViewAboveAppSupport.translatesAutoresizingMaskIntoConstraints = false
+        blankViewAboveSignOut.translatesAutoresizingMaskIntoConstraints = false
+        blankViewAboveAppSupport.addBorders(edges: [.top], color: UIColor(red: 0, green: 0, blue: 0, alpha: 0.37))
+        blankViewAboveSignOut.addBorders(edges: [.top], color: UIColor(red: 0, green: 0, blue: 0, alpha: 0.37))
+    }
+
+}
+
+extension UIView {
+    @discardableResult
+    func addBorders(edges: UIRectEdge,
+                    color: UIColor,
+                    inset: CGFloat = 0.0,
+                    thickness: CGFloat = 1.0) -> [UIView] {
+
+        var borders = [UIView]()
+
+        @discardableResult
+        func addBorder(formats: String...) -> UIView {
+            let border = UIView(frame: .zero)
+            border.backgroundColor = color
+            border.translatesAutoresizingMaskIntoConstraints = false
+            addSubview(border)
+            addConstraints(formats.flatMap {
+                NSLayoutConstraint.constraints(withVisualFormat: $0,
+                                               options: [],
+                                               metrics: ["inset": inset, "thickness": thickness],
+                                               views: ["border": border]) })
+            borders.append(border)
+            return border
+        }
+
+
+        if edges.contains(.top) || edges.contains(.all) {
+            addBorder(formats: "V:|-0-[border(==thickness)]", "H:|-inset-[border]-inset-|")
+        }
+
+        if edges.contains(.bottom) || edges.contains(.all) {
+            addBorder(formats: "V:[border(==thickness)]-0-|", "H:|-inset-[border]-inset-|")
+        }
+
+        if edges.contains(.left) || edges.contains(.all) {
+            addBorder(formats: "V:|-inset-[border]-inset-|", "H:|-0-[border(==thickness)]")
+        }
+
+        if edges.contains(.right) || edges.contains(.all) {
+            addBorder(formats: "V:|-inset-[border]-inset-|", "H:[border(==thickness)]-0-|")
+        }
+
+        return borders
+    }
 }
